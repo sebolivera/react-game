34c36f6ac13c3627979f45d9c30b252d5398a848
solivera385 <solivera@matrice.io>
2021-12-21 15:44:23 +0100

    switching to class-based cuz why not

--- src/Game.js
+++ src/Game.js
@@ -1,205 +1,280 @@
-import React, {useEffect, useRef} from 'react';
+import React, { createRef, Component } from 'react';
+import update from 'react-addons-update'; // ES6
 
-function Game(props)
+class Game extends Component
 {
-    const width = 1000;
-    const height = 600;
-    const canvas = useRef();
-    const tileWidth = 50;
-    const speedStep = 1;
-    const state = {
+    width = 1000;
+    height = 600;
+    canvas = createRef();
+    tileWidth = 50;
+    speedStep = 1;
+    state = {
         square: {
-            x : 500,
+            x: 500,
             y: 300,
-            accelX : 0,
-            accelY : 0,
-            deltaX : 0,
-            deltaY : 0,
-            towards : {
-                x : 500,
-                y : 300,
+            accelX: 0,
+            accelY: 0,
+            deltaX: 0,
+            deltaY: 0,
+            towards: {
+                x: 500,
+                y: 300,
             }
         }
     }
 
-    const draw = (context) => {
+    constructor(props)
+    {
+        super(props);
+        this.width = 1000;
+        this.height = 600;
+        this.canvas = createRef();
+        this.tileWidth = 50;
+        this.speedStep = 1;
+        this.state = {
+            square: {
+                x: 500,
+                y: 300,
+                accelX: 0,
+                accelY: 0,
+                deltaX: 0,
+                deltaY: 0,
+                towards: {
+                    x: 500,
+                    y: 300,
+                }
+            }
+        }
+    }
+    draw = (context) =>
+    {
         context.beginPath();
-        context.fillStyle= "white";
-        context.fillRect(0, 0, width, height);
+        context.fillStyle = "white";
+        context.fillRect(0, 0, this.width, this.height);
         context.lineWidth = 2;
 
-        for (let i = 0; i<=height; i+=tileWidth)
+        for (let i = 0; i <= this.height; i += this.tileWidth)
         {
             context.moveTo(0, i);
-            context.lineTo(width, i)
+            context.lineTo(this.width, i)
         }
         context.stroke();
 
-        for (let i = 0; i<=width; i+=tileWidth)
+        for (let i = 0; i <= this.width; i += this.tileWidth)
         {
             context.moveTo(i, 0);
-            context.lineTo(i, height)
+            context.lineTo(i, this.height)
         }
         context.stroke();
-        
-        context.fillStyle= "red";
-        context.fillRect(state.square.x-tileWidth, state.square.y-tileWidth, tileWidth, tileWidth);
+
+        context.fillStyle = "red";
+        context.fillRect(this.state.square.x - this.tileWidth, this.state.square.y - this.tileWidth, this.tileWidth, this.tileWidth);
     }
 
 
-    const move = (event) =>
+    move = (event) =>
     {
-        if (state.square.x === state.square.towards.x && state.square.y === state.square.towards.y)
+        console.log('keypress:', event.keyCode);
+        console.log('state:', this.state);
+        let say = this.state.square.accelY;
+        let sax = this.state.square.accelX;
+        let sty = this.state.square.towards.y;
+        let stx = this.state.square.towards.x;
+        if (this.state.square.x === this.state.square.towards.x && this.state.square.y === this.state.square.towards.y)
         {
             switch (event.keyCode)
             {
                 case 40://up
-                    if (state.square.towards.y < height-tileWidth)
-                        state.square.towards.y += tileWidth;
-                        state.square.accelY = tileWidth/2;
+                    if (this.state.square.towards.y < this.height - this.tileWidth)
+                    {
+                        sty += this.tileWidth;
+                        say = this.tileWidth/2;
+                    }
                     break;
                 case 38://down
-                    if (state.square.towards.y > tileWidth)
-                        state.square.towards.y -= tileWidth;
-                        state.square.accelY = tileWidth/2;
+                    if (this.state.square.towards.y > this.tileWidth)
+                    {
+                        sty -= this.tileWidth;
+                        say = this.tileWidth/2;
+                    }
                     break;
                 case 39://left
-                    if (state.square.towards.x < width-tileWidth)
-                        state.square.towards.x += tileWidth;
-                        state.square.accelX = tileWidth/2;
+                    if (this.state.square.towards.x < this.width - this.tileWidth)
+                    {
+                        stx += this.tileWidth;
+                        sax = this.tileWidth/2;
+                    }
                     break;
                 case 37://right
-                    if (state.square.towards.x > tileWidth)
-                        state.square.towards.x -= tileWidth;
-                        state.square.accelX = tileWidth/2;
+                    if (this.state.square.towards.x > this.tileWidth)
+                    {
+                        stx -= this.tileWidth;
+                        sax = this.tileWidth/2;
+                    }
                     break;
                 default:
                     break;
             }
+            console.log('stx:',stx);
+            this.setState({ square: update(this.state.square, {towards: {x: {$set: stx}}})});// isn't there a shorter way to do this ???
+            console.log('what:', this.state);
+            this.setState({ square: update(this.state.square, {towards: {y: {$set: sty}}})});
+            this.setState({ square: update(this.state.square, {accelX: {$set: sax}})});
+            this.setState({ square: update(this.state.square, {accelY: {$set: say}})});
+            
         }
     }
 
-    const updatesquareaccel = (step=1) => 
-    {
-        if (state.square.x !== state.square.towards.x)
+    updatesquareaccel = (step = 1) =>
+    {//I am so fucking sorry for this
+        let ssx = this.state.square.x;
+        let sdx = this.state.square.deltaX;
+        let sax = this.state.square.accelX;
+        let sty = this.state.square.towards.y;
+        let stx = this.state.square.towards.x;
+        let ssy = this.state.square.y;
+        let sdy = this.state.square.deltaY;
+        let say = this.state.square.accelY;
+        if (ssx !== stx)
         {
             let stepX = step;
-            if (Math.abs(state.square.x  - state.square.towards.x) < tileWidth/2)
+            if (Math.abs(ssx - stx) < this.tileWidth / 2)
             {
-                state.square.accelX = 0;
+                sax = 0;
             }
-            if (state.square.accelX > state.square.deltaX)
+            this.setState({ square: update(this.state.square, {accelX: {$set: sax}})});
+            if (sax > sdx)
             {
-                state.square.deltaX += stepX;
+                sdx += stepX;
             }
             else
-            {
-                state.square.deltaX -= stepX;
-                if (state.square.deltaX < 0)
-                    state.square.deltaX = 0;
+            {  
+                sdx -= stepX;
+                if (sdx < 0){
+                    sdx = 0;
+                }
             }
 
-            if (state.square.x < state.square.towards.x)
+            if (ssx < stx)
             {
-                if (state.square.x + state.square.deltaX < state.square.towards.x && state.square.deltaX > 0)
+                if (ssx + sdx < stx && sdx > 0)
                 {
-                    state.square.x += state.square.deltaX;
+                    ssx += sdx;
                 }
                 else
                 {
-                    state.square.x = state.square.towards.x;
+                    ssx = stx;
                 }
             }
-            else if (state.square.x > state.square.towards.x)
+            else if (ssx > stx)
             {
-                
-                if (state.square.x - state.square.deltaX >= state.square.towards.x && state.square.deltaX > 0)
+                if (ssx - sdx >= stx && sdx > 0)
                 {
-                    state.square.x -= state.square.deltaX;
+                    ssx -= sdx;
                 }
                 else
                 {
-                    state.square.x = state.square.towards.x;
+                    ssx = stx;
                 }
             }
-            
-            if (state.square.x === state.square.towards.x)
+
+            this.setState({ square: update(this.state.square, {x: {$set: ssx}})});
+            if (ssx === stx)
             {
-                state.square.accelX = 0;
-                state.square.deltaX = 0;
+                sax = 0;
+                sdx = 0;
             }
+            this.setState({ square: update(this.state.square, {accelX: {$set: sax}})});
+            this.setState({ square: update(this.state.square, {deltaX: {$set: sdx}})});
         }
-        if(state.square.y !== state.square.towards.y)
+
+        // Y axis
+        if (ssy !== sty)
         {
             let stepY = step;
-            if (Math.abs(state.square.y - state.square.towards.y) < tileWidth/2)
+            if (Math.abs(ssy - sty) < this.tileWidth / 2)
             {
-                state.square.accelY = 0;
+                say = 0;
             }
-            if (state.square.accelY > state.square.deltaY)
+            this.setState({ square: update(this.state.square, {accelY: {$set: say}})});
+            if (say > sdy)
             {
-                state.square.deltaY += stepY;
+                sdy += stepY;
             }
             else
-            {
-                state.square.deltaY -= stepY;
-                if (state.square.deltaY < 0)
-                    state.square.deltaY = 0;
+            {  
+                sdy -= stepY;
+                if (sdy < 0){
+                    sdy = 0;
+                }
             }
 
-            if (state.square.y < state.square.towards.y)
+            if (ssy < sty)
             {
-                if (state.square.y + state.square.deltaY < state.square.towards.y && state.square.deltaY > 0)
+                if (ssy + sdy < sty && sdy > 0)
                 {
-                    state.square.y += state.square.deltaY;
+                    ssy += sdy;
                 }
                 else
                 {
-                    state.square.y = state.square.towards.y;
+                    ssy = sty;
                 }
             }
-            else if (state.square.y > state.square.towards.y)
+            else if (ssy > sty)
             {
-                if (state.square.y - state.square.deltaY > state.square.towards.y && state.square.deltaY > 0)
+                if (ssy - sdy >= sty && sdy > 0)
                 {
-                    state.square.y -= state.square.deltaY;
+                    ssy -= sdy;
                 }
                 else
                 {
-                    state.square.y = state.square.towards.y;
+                    ssy = sty;
                 }
             }
-            
-            if (state.square.y === state.square.towards.y)
+
+            this.setState({ square: update(this.state.square, {y: {$set: ssy}})});
+            if (ssy === sty)
             {
-                state.square.accelY = 0;
-                state.square.deltaY = 0;
+                say = 0;
+                sdy = 0;
             }
+            this.setState({ square: update(this.state.square, {accelY:{$set: say}})});
+            console.log('set sdy as', sdy)
+            this.setState({ square: update(this.state.square, {deltaY:{$set: sdy}})});
         }
-        
+
     }
 
-    const update = () => 
+    update = () =>
     {
-        updatesquareaccel(speedStep);
+        this.updatesquareaccel(this.speedStep);
     }
 
-    useEffect(() =>
+    componentDidMount()
     {
-        document.addEventListener("keydown", move, true);
-        const context = canvas.current.getContext("2d");
-        setInterval(() => {
-            update();
-            draw(context);
-        }, 1000/60);
-    });
+        document.addEventListener("keydown", this.move, true);
+        const context = this.canvas.current.getContext("2d");
+        // this.update();
+        // this.draw(context);
+        setInterval(() =>
+        {
+            this.update();
+            this.draw(context);
+        }, 1000 / 60);
+    }
 
+    componentDidUpdate()//not really sure what I'll put there yet
+    {
+    };
 
-    return (
-        <div>
-            <canvas ref={canvas} width={width} height={height}/>
-        </div>
-    )
+    render()
+    {
+        return (
+            <div>
+                <canvas ref={this.canvas} width={this.width} height={this.height} />
+            </div>
+        )
+    }
 }
 
 export default Game;
